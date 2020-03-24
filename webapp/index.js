400467fec2b70c4031cf1bdcf1a85b641a1b2092
T. Gaspar Da Silva <t.gaspar.da.silva@avanade.com>
2020-03-24 20:06:22 -0300

    Aggregation Binding Using a Factory Function

--- webapp/index.js
+++ webapp/index.js
@@ -1,44 +1,40 @@
 sap.ui.require([
 	"sap/ui/model/json/JSONModel",
 	"sap/ui/core/mvc/XMLView",
-	"sap/ui/model/BindingMode",
 	"sap/ui/model/resource/ResourceModel"
-], function (JSONModel, XMLView, BindingMode, ResourceModel) {
+], function (JSONModel, XMLView, ResourceModel) {
 	"use strict";
 
 	// Attach an anonymous function to the SAPUI5 'init' event
 	sap.ui.getCore().attachInit(function () {
-        var oProductModel = new JSONModel();
+
+		var oProductModel = new JSONModel();
 		oProductModel.loadData("./model/Products.json");
-        sap.ui.getCore().setModel(oProductModel, "products");
-        
-		// Create a JSON model from an object literal
+		sap.ui.getCore().setModel(oProductModel, "products");
+
 		var oModel = new JSONModel({
 			firstName: "Harry",
 			lastName: "Hawk",
 			enabled: true,
-			panelHeaderText: "Data Binding Basics",
 			address: {
 				street: "Dietmar-Hopp-Allee 16",
 				city: "Walldorf",
 				zip: "69190",
 				country: "Germany"
 			},
-            "salesToDate" : 12345.6789,
-            "priceThreshold": 20,
-			"currencyCode" : "EUR"
+			"salesToDate": 12345.6789,
+			"priceThreshold": 20,
+			"currencyCode": "EUR"
 		});
-		// Assign the model object to the SAPUI5 core
-        sap.ui.getCore().setModel(oModel);
-        //oModel.setDefaultBindingMode(BindingMode.OneWay);
 
-        // Create a resource bundle for language specific texts
-		var oResourceModel = new ResourceModel({
+		// Assign the model object to the SAPUI5 core
+		sap.ui.getCore().setModel(oModel);
+
+		var oResourceBundle = new ResourceModel({
 			bundleName: "sap.ui.demo.db.i18n.i18n"
 		});
 
-		// Assign the model object to the SAPUI5 core using the name "i18n"
-		sap.ui.getCore().setModel(oResourceModel, "i18n");
+		sap.ui.getCore().setModel(oResourceBundle, "i18n");
 
 		// Display the XML view called "App"
 		var oView = new XMLView({
@@ -46,9 +42,9 @@ sap.ui.require([
 		}).placeAt("content");
 
 		// Register the view with the message manager
-        sap.ui.getCore().getMessageManager().registerObject(oView, true);
-        
-        // Insert the view into the DOM
+		sap.ui.getCore().getMessageManager().registerObject(oView, true);
+
+		// Insert the view into the DOM
 		oView.placeAt("content");
 	});
 });
